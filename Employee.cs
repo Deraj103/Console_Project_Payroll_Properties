652222fe7a9a0b4c7fbc216e59ea26140b14c15b
Jared Tims <jstims9109@gmail.com>
2024-07-03 20:55:54 -0600

    Add project files.

---
+++ Employee.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Console;
+
+namespace Console_Project_Payroll_Properties
+{
+    class Employee
+    {
+        // auto-implemented properties
+        private string FirstName { get; set; }
+        private string LastName { get; set; }
+        private int EmployeeID { get; set; }
+        // constructor
+        public Employee(string fName, string lName, int empID)
+        {
+            FirstName = fName;
+            LastName = lName;
+            EmployeeID = empID;
+        }
+        // tostring method
+        public override string ToString()
+        {
+            return ($"Employee {EmployeeID}: {FirstName} {LastName}, ");
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Console;
+
+namespace Console_Project_Payroll_Properties
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            WriteLine("Console Project Payroll with Properties\n");
+
+            Write("\nPress any key to continue...");
+            ReadKey();
+        }
+    }
+}
